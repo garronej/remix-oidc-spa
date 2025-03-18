a00a240ac7f17d30c2d17c7d14a5960c653c977c
garronej <joseph.garrone@protonmail.com>
2025-03-18 08:27:59 +0100

    Prepare for Vercel deployment

--- app/oidc.client.ts
+++ app/oidc.client.ts
@@ -2,7 +2,7 @@ import { createReactOidc } from "oidc-spa/react";
 
 // Docs: https://www.oidc-spa.dev/
 export const { OidcProvider, useOidc, getOidc } = createReactOidc({
-  issuerUri: import.meta.env.VITE_OIDC_ISSUER,
+  issuerUri: import.meta.env.VITE_OIDC_ISSUER_URI,
   clientId: import.meta.env.VITE_OIDC_CLIENT_ID,
   homeUrl: import.meta.env.BASE_URL,
   autoLogin: true,
